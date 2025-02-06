79493d3321e2125bec310889076e3acafb593156
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-02-06 16:51:38 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
 
 
 const page = () => {
   return (
-    <div className="text-red-200 pt-6 pl-5 bg-white pb-5 ">page</div>
+    <div className="text-red-200 pt-6 pl-5 bg-white pb-5 ">
+      <Input></Input>
+      <Button>teste</Button>
+    </div>
   )
 }
 
