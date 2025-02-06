a139423ff169b49221615c47d12942f3c362900a
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-02-06 16:58:55 -0300

    chore: update shadcn theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from "./_components/ui/button"
-import { Input } from "./_components/ui/input"
 
 
 const page = () => {
   return (
-    <div className="text-red-200 pt-6 pl-5 bg-white pb-5 ">
-      <Input></Input>
+    <div className="">
       <Button>teste</Button>
     </div>
   )
