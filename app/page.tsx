17517ccc75e70d97f21be631239bcd93736dd32d
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-02-06 22:19:07 -0300

    feat: add header components and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,35 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 
 const page = () => {
   return (
-    <div className="">
-      <Button>teste</Button>
-    </div>
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Jonathan!</h2>
+        <h3>Sexta, 2 de Fevereiro</h3>
+      </div>
+
+      <div className="p-5 flex items-center gap-2">
+        <Input placeholder="Buscar" />
+        <Button >
+          <SearchIcon />
+        </Button>
+      </div>
+
+      <div className="relative h-[150px] mt-5 m-5 ">
+        <Image
+          alt="Agende com os melhores"
+          src="/Banner01.png"
+          fill
+          className="object-fill rounded-xl"
+        />
+      </div>
+    </div >
   )
 }
 
