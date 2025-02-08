b3994980929a9e746affcb6be5e06a601d295b8c
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-02-08 18:01:07 -0300

    feat: add BookingItem component

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,10 @@ import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constants/searchIcons"
+import BookingItem from "./_components/booking-item"
 
 
 const page = async () => {
@@ -57,29 +56,8 @@ const page = async () => {
         </div>
 
         {/* AGENDAMENTOS */}
+        <BookingItem />
 
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">agendamentos</h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
-              <p className="text-sm">Fevereiro</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
 
         {/* RECOMENDADOS */}
 
