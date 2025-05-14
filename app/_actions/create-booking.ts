b1e755fb0655c3ef11b5f64070180e7e3755a577
Jonathan Costa <jonathanduartecosta@gmail.com>
2025-05-13 21:23:57 -0300

    feat: ensure user authentic

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -1,6 +1,8 @@
 'use server'
 
+import { getServerSession } from "next-auth"
 import { db } from "../_lib/prisma"
+import { authOptions } from "../_lib/auth"
 
 interface createBookingParams {
   userId: string
@@ -8,6 +10,10 @@ interface createBookingParams {
   date: Date
 }
 export const createBooking = async (params: createBookingParams) => {
+  const user = await getServerSession(authOptions)
+  if(!user) {
+    throw new Error("usuário não autenticado")
+  }
   await db.booking.create({
     data: params,
   })
